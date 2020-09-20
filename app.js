4af18324b83b27360f658acdd00f2787522e8ff4
Sangha Yoon <yoons4@spu.edu>
2020-09-20 13:17:35 -0700

    Finished app.js

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
-const http = require('http');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const port = process.env.PORT || 5000;
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-const server = http.createServer((request, response) => {
-			response.statusCode = 200;
-			response.setHeader('Content-Type', 'text/plain');
-			response.end('Welcome to my Web Application\n\nMy name is: Sangha Yoon');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-server.listen(port, (err) => {
-	if(err){
-		return console.log('Something went wrong ', err);
-	}
-	console.log(`Server Listening on: ${port}`);
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 5000);
+  res.render('error');
 });
+
+module.exports = app;
